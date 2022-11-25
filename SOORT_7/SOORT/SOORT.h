9ebf7d52e719cec1df759636603ead1459a444d0
verrka <105545580+verrka@users.noreply.github.com>
2022-11-25 20:50:28 +0100

    Add files via upload

---
+++ SOORT_7/SOORT/SOORT.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef SOORT_H
+#define SOORT_H
+
+class rs_class {
+	int getMax(int arr[], int n);
+	void countSort(int arr[], int n, int exp);
+	void radixSort(int arr[], int n);
+};
+#endif // !SOORT_H
+
---
+++ SORT_7/SORT/SORT.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef SORT_H
+#define SORT_H
+
+class cs_class {
+	void countSort(int arr[], int n);
+	void display(int arr[], int n);
+};
+
+
+
+
+#endif // SORT_H
+
